62bfaa81522c66d53415df330678a2973153ee76
Ian Restrepo <ianrestrepog@gmail.com>
2024-12-21 10:01:16 -0500

    NEW | Netlify server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,12 @@ import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
 
 
+import netlify from '@astrojs/netlify';
+
+
 // https://astro.build/config
-export default defineConfig({integrations: [db()]});
+export default defineConfig({
+  integrations: [db()],
+  output: 'server',
+  adapter: netlify()
+});
